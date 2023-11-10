b317d3f67ffac3fdf7c3488aed085622043c04f8
gadc1996 <gadc1996@gmail.com>
2023-11-10 20:48:24 +0000

    update installation requirements

--- commands/setup.py
+++ commands/setup.py
@@ -7,13 +7,6 @@ import shutil
 @click.command()
 def setup():
     """Setup project dependencies and configurations"""
-    if not os.path.exists(".env"):
-        click.secho("No env file detected, copying example file", fg="red")
-        shutil.copy(".env.example", ".env")
-        click.secho("Env file created", fg="green")
-    else:
-        click.secho("Env file detected", fg="green")
-
     subprocess.run(["pre-commit", "install"], check=True)
 
     subprocess.run(["cli", "db", "migrate"])
